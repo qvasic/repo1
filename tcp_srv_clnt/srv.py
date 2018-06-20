24ba0f4859dcbf0293b621d9d0dbec1493aadb1d
Vasyl Demianov <qvasic@gmail.com>
2018-06-20 09:57:29 +0300

    server and client now can be run with devices

--- tcp_srv_clnt/srv.py
+++ tcp_srv_clnt/srv.py
@@ -1,26 +1,32 @@
 import socket
 import socketserver
 
-PORT = 10443
+PORT = 2222
 FILE = "gps.log"
 FREQ = 10
 
 class CCPRequestHandler( socketserver.StreamRequestHandler ):
     def handle( self ):
-        import time
-
-        print( "connection from {}".format( self.client_address ) )
-        print( "sending lines from {} with frequency {}".format( FILE, FREQ ) )
-
-        with open( FILE, "rb" ) as f:
-            for l in f:
-                self.wfile.write( l )
-                time.sleep( 1/FREQ )
+        try:
+            import time
+            print( "connection from {}".format( self.client_address ) )
+            print( "sending lines from {} with frequency {}".format( FILE, FREQ ) )
+            lines_sent = 0
+            with open( FILE, "rb" ) as f:
+                for l in f:
+                    self.wfile.write( l )
+                    lines_sent += 1
+                    print( "\r{} lines sent".format( lines_sent ), end="" )
+                    time.sleep( 1/FREQ )
+                print()
+        except KeyboardInterrupt:
+            self.server.shutdown()
 
 def main():
     srv = socketserver.TCPServer( ( "", PORT ), CCPRequestHandler )
     print( "opened server on port {}, serving forever".format( PORT ) )
     srv.serve_forever()
+    srv.server_close()
 
 if __name__ == "__main__":
     main()
