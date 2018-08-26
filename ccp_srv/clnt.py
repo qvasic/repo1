96242857910a4a76d945f042e2ebc3869acee9d1
Vasyl Demianov <VasylDemianov@gmail.com>
2018-08-26 10:03:53 +0300

    clnt.py now takes ip address from command line

--- ccp_srv/clnt.py
+++ ccp_srv/clnt.py
@@ -3,9 +3,11 @@
 def main():
     print( __file__, __doc__ )
 
-    import socket
+    import socket, sys
 
-    s = socket.create_connection( ( "localhost", 2222 ) )
+    addr = "localhost" if len( sys.argv ) == 1 else sys.argv[1]
+
+    s = socket.create_connection( ( addr, 2222 ) )
     print( "reading from server" )
     for l in s.makefile( 'r' ):
         print( l, end="" )
