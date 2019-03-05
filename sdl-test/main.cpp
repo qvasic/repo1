c0d4633617c84852dcdbca13edf05dfcabce6260
Vasyl Demianov <VasylDemianov@gmail.com>
2019-03-05 11:38:42 +0200

    SDL-TEST: working with a joystick finished, can now read axis values

--- sdl-test/main.cpp
+++ sdl-test/main.cpp
@@ -2,16 +2,73 @@
 
 #include <chrono>
 #include <iostream>
+#include <iomanip>
 #include <thread>
+
+void
+show_joystick( )
+{
+    int joystick_count = SDL_NumJoysticks( );
+
+    if ( SDL_NumJoysticks( ) < 1 )
+    {
+        std::cerr << "no joystick present in the system to show" << std::endl;
+        return;
+    }
+
+    std::cout << "there are " << joystick_count << " joystick(s) present: " << std::endl;
+    for ( int i = 0; i < joystick_count; ++i )
+    {
+        std::cout << i+1 << " " << SDL_JoystickNameForIndex( i ) << std::endl;
+    }
+
+    SDL_Joystick* joystick = SDL_JoystickOpen( 0 );
+
+    if ( joystick == nullptr )
+    {
+        std::cerr << "couldn't open joystick" << std::endl;
+        return;
+    }
+
+    int axes_count = SDL_JoystickNumAxes( joystick );
+    std::cout << "opened joystick " << SDL_JoystickName( joystick )
+              << ", it has " << axes_count << " axes" << std::endl;
+
+    bool exit_event = false;
+    for ( ; !exit_event; )
+    {
+        SDL_Event event;
+        while ( SDL_PollEvent( &event ) )
+        {
+            if ( event.type == SDL_JOYBUTTONDOWN )
+            {
+                exit_event = true;
+            }
+        }
+
+        std::cout << "\raxes: " << std::setprecision( 2 );
+        for ( int i=0; i < axes_count; ++i )
+        {
+            std::cout << SDL_JoystickGetAxis( joystick, i ) / double( 0xefff ) << ' ';
+        }
+        std::cout << "      ";
+
+        std::this_thread::sleep_for( std::chrono::milliseconds( 50 ) );
+    }
+
+    SDL_JoystickClose( joystick );
+}
 
 int
 main( int, char** )
 {
-    SDL_Init( SDL_INIT_JOYSTICK );
+    if ( SDL_Init( SDL_INIT_JOYSTICK ) < 0 )
+    {
+        std::cout << "COULDN'T INIT SDL: " << SDL_GetError( ) << std::endl;
+        return -1;
+    }
 
-    std::cout << __FILE__ << std::endl;
-    std::this_thread::sleep_for( std::chrono::seconds( 1 ) );
-    printf( "the system has %d joysticks present\n", SDL_NumJoysticks( ) );
+    show_joystick( );
 
     SDL_Quit( );
 }
