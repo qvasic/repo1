ff05836b6d601d5b3debfd68fabe19a9ea737be3
Vasyl Demianov <VasylDemianov@gmail.com>
2019-04-09 07:39:43 +0300

    MULTI-INHERIT-VIRTUALS: multi inherited listener added

--- multi_inherit_virtuals/main.cpp
+++ multi_inherit_virtuals/main.cpp
@@ -8,6 +8,7 @@
 #include <atomic>
 #include <mutex>
 #include <vector>
+#include <algorithm>
 
 class MyConsoleReaderListener : public ConsoleReaderListener
 {
@@ -27,12 +28,42 @@ private:
     vdem::CommandLineInterface& m_cli;
 };
 
+ class ConsoleReaderListenerWithStorage : public std::vector< std::string >
+                                        , public ConsoleReaderListener
+ {
+ public:
+     ConsoleReaderListenerWithStorage( vdem::CommandLineInterface& cli )
+         : m_cli( cli )
+     {
+     }
+
+     void
+     on_new_line_read( const std::string& new_line ) override
+     {
+         this->push_back( new_line );
+         std::string message = "NEW LINE RECIEVED, SO FAR WE'VE GOT THIS:";
+         std::for_each( std::begin( *this ), std::end( *this ),
+                        [ &message ]( const std::string& str )
+                        {
+                            message += " \"" + str + "\"";
+                        }
+            );
+
+         m_cli.print_message( message );
+     }
+
+ private:
+    vdem::CommandLineInterface& m_cli;
+ };
+
 int main( )
 {
     vdem::CommandLineInterface command_line;
     ConsoleReader console_reader( command_line );
     MyConsoleReaderListener listener1( command_line );
+    ConsoleReaderListenerWithStorage listener2( command_line );
     console_reader.add_listener( &listener1 );
+    console_reader.add_listener( &listener2 );
 
     std::atomic< bool > stop ( false );
     auto status_routine = [ &command_line, &stop ]( )
