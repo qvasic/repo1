5d829f2730a9578732fe15fa273887d37251c8e6
Vasyl Demianov <qvasic@gmail.com>
2020-05-22 18:10:07 +0300

    c++test/sdl-test2 now compiles and run on Windows.

--- c++tests/sdl-test-2/main.cpp
+++ c++tests/sdl-test-2/main.cpp
@@ -2,19 +2,64 @@
 #include <iostream>
 #include <exception>
 
-int main()
-try
+namespace sdl
 {
-    std::cout << "one" << std::endl;
-    if ( SDL_Init( SDL_INIT_VIDEO ) != 0 )
+
+void
+create_window( )
+{
+
+    std::cout << "SDL window created, now closing it." << std::endl;
+
+
+}
+
+class Library
+{
+public:
+    Library( Uint32 flags )
     {
-        std::cout << "two" << std::endl;
-        throw std::runtime_error( std::string( "Could not initialize SDL video system: " ) + SDL_GetError( ) );
+        if ( SDL_Init( flags ) != 0 )
+        {
+            throw std::runtime_error( std::string( "Could not initialize SDL video system: " ) + SDL_GetError( ) );
+        }
+    }
+    ~Library()
+    {
+        SDL_Quit( );
+    }
+};
+
+class Window
+{
+public:
+    Window( const char *title, int x, int y, int w, int h, Uint32 flags )
+        : m_sdl_window( SDL_CreateWindow( title, x, y, w, h, flags ) )
+    {
+        if ( m_sdl_window == nullptr )
+        {
+            throw std::runtime_error( std::string( "Could not create SDL window: " ) + SDL_GetError( ) );
+        }
     }
 
-    std::cout << "thee" << std::endl;
-    SDL_Quit( );
-    std::cout << "four" << std::endl;
+    ~Window()
+    {
+        SDL_DestroyWindow( m_sdl_window );
+    }
+
+private:
+    SDL_Window* m_sdl_window;
+};
+
+}
+
+extern "C" int
+SDL_main( int, char** )
+try
+{
+    sdl::Library lib( SDL_INIT_VIDEO );
+    sdl::Window wnd( "asdfasdf", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                     800, 600, 0 );
     return 0;
 }
 catch( const std::exception& e)
