cc2211931f305c5e63a4227bdead91989cf0e3d8
Vasyl Demianov <qvasic@gmail.com>
2020-05-23 16:17:38 +0300

    Rendering a line at mouse position added.

--- c++tests/sdl-test-2/main.cpp
+++ c++tests/sdl-test-2/main.cpp
@@ -23,35 +23,88 @@ public:
     }
 };
 
+class Renderer
+{
+public:
+    explicit Renderer( SDL_Renderer* sdl_renderer_ptr )
+        : m_sdl_renderer_ptr( sdl_renderer_ptr )
+    {}
+
+    ~Renderer( )
+    {
+        SDL_DestroyRenderer( m_sdl_renderer_ptr );
+    }
+
+
+    void clear( )
+    {
+        SDL_RenderClear( m_sdl_renderer_ptr );
+    }
+
+    void present( )
+    {
+        SDL_RenderPresent( m_sdl_renderer_ptr );
+    }
+
+    void set_draw_color( Uint8 r, Uint8 g, Uint8 b, Uint8 a )
+    {
+        SDL_SetRenderDrawColor( m_sdl_renderer_ptr, r, g, b, a );
+    }
+
+    void draw_line( int x1, int y1, int x2, int y2 )
+    {
+        SDL_RenderDrawLine( m_sdl_renderer_ptr, x1, y1, x2, y2 );
+    }
+
+private:
+    SDL_Renderer* m_sdl_renderer_ptr;
+};
+
 class Window
 {
 public:
     Window( const char *title, int x, int y, int w, int h, Uint32 flags )
-        : m_sdl_window( SDL_CreateWindow( title, x, y, w, h, flags ) )
+        : m_sdl_window_ptr( SDL_CreateWindow( title, x, y, w, h, flags ) )
     {
-        if ( m_sdl_window == nullptr )
+        if ( m_sdl_window_ptr == nullptr )
         {
             throw std::runtime_error( std::string( "Could not create SDL window: " ) + SDL_GetError( ) );
         }
     }
 
+
+
+
+
     SDL_Surface* get_surface( )
     {
-        return SDL_GetWindowSurface( m_sdl_window );
+        return SDL_GetWindowSurface( m_sdl_window_ptr );
     }
 
     void update_window_surface( )
     {
-        SDL_UpdateWindowSurface( m_sdl_window);
+        SDL_UpdateWindowSurface( m_sdl_window_ptr );
     }
+
+    Renderer create_accelerated_renderer( )
+    {
+        auto* sdl_renderer_ptr = SDL_CreateRenderer( m_sdl_window_ptr, -1, SDL_RENDERER_ACCELERATED );
+        if ( sdl_renderer_ptr == nullptr )
+        {
+            throw std::runtime_error( std::string( "Could not create SDL renderer: " ) + SDL_GetError( ) );
+        }
+        return Renderer( sdl_renderer_ptr );
+    }
+
+
 
     ~Window()
     {
-        SDL_DestroyWindow( m_sdl_window );
+        SDL_DestroyWindow( m_sdl_window_ptr );
     }
 
 private:
-    SDL_Window* m_sdl_window;
+    SDL_Window* m_sdl_window_ptr;
 };
 
 }
@@ -60,22 +113,21 @@ extern "C" int
 SDL_main( int, char** )
 try
 {
-    sdl::Library lib( SDL_INIT_VIDEO );
-    sdl::Window wnd( "sdl-test2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                     800, 600, 0 );
+    sdl::Library library( SDL_INIT_VIDEO );
+    sdl::Window window( "sdl-test2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                        800, 600, 0 );
 
-    auto* surface = wnd.get_surface( );
-    SDL_Rect rect{ 10, 10, 200, 100 };
-    SDL_FillRect( surface, &rect, SDL_MapRGB( surface->format, 128, 64, 0 ) );
-    SDL_FreeSurface( surface );
-    wnd.update_window_surface( );
-
+    auto renderer = window.create_accelerated_renderer( );
 
     SDL_Event event;
     bool quit = false;
+    bool redraw = false;
+
+    int x = 0, y = 0;
 
     for ( ;; )
     {
+        redraw = false;
 
         while ( SDL_PollEvent( &event ) )
         {
@@ -83,13 +135,30 @@ try
             {
                 quit = true;
             }
+            else if ( event.type = SDL_MOUSEMOTION )
+            {
+                x = event.motion.x;
+                y = event.motion.y;
+                redraw = true;
+            }
         }
-
-        std::this_thread::sleep_for( std::chrono::milliseconds( 30 ) );
 
         if ( quit )
         {
             break;
+        }
+
+        if ( redraw )
+        {
+            renderer.set_draw_color( 255, 255, 255, SDL_ALPHA_OPAQUE );
+            renderer.clear( );
+            renderer.set_draw_color( 0, 0, 0, SDL_ALPHA_OPAQUE );
+            renderer.draw_line( 0, 0, x, y );
+            renderer.present( );
+        }
+        else
+        {
+            std::this_thread::sleep_for( std::chrono::milliseconds( 30 ) );
         }
     }
 
