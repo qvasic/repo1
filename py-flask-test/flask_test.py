49cdefdc3cf05e0a4eeea42ed08c1cccb0c75f2a
Vasyl Demianov <ext-vasyl.demianov@here.com>
2020-07-19 23:21:51 +0300

    Test flask app improved that it actually sends data to srv.

--- py-flask-test/flask_test.py
+++ py-flask-test/flask_test.py
@@ -8,6 +8,10 @@ def hell0( ):
 	if flask.request.method == 'GET':
 		return flask.render_template( "search.html" )
 	else:
-		return "search result will be here, url args: {} form: {}, files: {}".format( len( flask.request.args ), 
-																					  len( flask.request.form ),
-																					  len( flask.request.files ) )
+		all_files = { f : ( flask.request.files[ f ].content_type, 
+		                    flask.request.files[ f ].stream.read( ) ) 
+						for f in flask.request.files }
+			
+		return "search result will be here, url args: {} form: {}, files: {}".format( dict( flask.request.args ), 
+																					  dict( flask.request.form ),
+																					  all_files )
